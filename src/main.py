8d68a694ff6446f8e604ebe6c198c29996ddbc9e
M4KiF_ <kkoterba9@gmail.com>
2023-01-09 22:40:30 +0100

    .

--- src/main.py
+++ src/main.py
@@ -14,12 +14,23 @@
 # Main app structure
 from packages.window import window_gui as gui
 
+import sys
+
 #################
 # Main function #
 #################
 
 def main():
     print("Is it okey?")
+
+    app = gui.QApplication(sys.argv)
+
+    window = gui.Window()
+    window.setFixedSize(1280,720)
+
+    window.show()
+
+    sys.exit(app.exec())
 
 if __name__ =="__main__":
     main()
